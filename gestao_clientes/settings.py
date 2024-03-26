afed756a418e6da717858436f07c6b44c3d416f0
diogobho <diogo_bho@hotmail.com>
2024-03-26 01:47:38 -0300

    change first deploy

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+static_root = os.path.join(BASE_DIR, 'staticfiles')
+staticfiles_dirs = [
+    os.path.join(BASE_DIR, 'static'),]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
